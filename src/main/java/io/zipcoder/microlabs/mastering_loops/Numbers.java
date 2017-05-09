4407a9ce1e5e106ff730225cebf1abbc5bb2d738
ant <anthonyjones@zipcoder3.local>
2017-05-09 11:36:38 -0400

    Finished onetoten through squares, working on randomtest now

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -3,27 +3,75 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Numbers {
 
-    public String oneToTen(){
+    public String oneToTen() {
+        String output = "*** Output ***\n";
+        String onToTenTitle = "oneToTen()\n";
+        String filledString = "";
+        for (int i = 1; i < 11; i++) {
+            if (i == 10) {
+                filledString += Integer.toString(i);
+            } else {
+                filledString += Integer.toString(i) + "\n";
+            }
+        }
+        return output + onToTenTitle + filledString;
+    }
+
+    public String oddNumbers() {
+
+        String output = "*** Output ***\n";
+        String oodNumbersTitle = "oodNumbers()\n";
+        String filledString = "";
+        for (int i = 1; i < 20; i++) {
+            if (i % 2 != 0) {
+                if (i == 19) {
+                    filledString += Integer.toString(i);
+                } else {
+                    filledString += Integer.toString(i) + "\n";
+                }
+            }
+        }
+        return output + oodNumbersTitle + filledString;
+    }
+
+    public String squares() {
+        String output = "*** Output ***\n";
+        String squareNumbersTitle = "squares()\n";
+        String filledString = "";
+        for (int i = 1; i < 11; i++) {
+
+            if (i == 10) {
+                filledString += Integer.toString((int) Math.pow(i, 2));
+            } else {
+                filledString += Integer.toString((int) Math.pow(i, 2)) + "\n";
+            }
+
+        }
+        return output + squareNumbersTitle + filledString;
+    }
+//must have mathrandom
+    public String random4() {
+        String output = "*** Output ***\n";
+        String squareNumbersTitle = "random4()\n";
+        String filledString = "";
+        double randomNumber =  Math.random()*10;
+
+
+
+        for (int i = 1; i < 11; i++) {
+
+            filledString +=  Integer.toString((int)randomNumber) + "\n";
+
+        }
+        return output + squareNumbersTitle + filledString;
+
+    }
+
+    public String even(int n) {
         return "";
     }
 
-    public String oddNumbers(){
-        return "";
-    }
-
-    public String squares(){
-        return "";
-    }
-
-    public String random4(){
-        return "";
-    }
-
-    public String even(int n){
-        return "";
-    }
-
-    public String powers(int n){
+    public String powers(int n) {
         return "";
     }
 
--- src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/NumbersTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class NumbersTest {
 
     @Test
-    public void oneToTenTest(){
+    public void oneToTenTest() {
         //: Given
         Numbers numbers = new Numbers();
         String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
@@ -19,7 +19,48 @@ public class NumbersTest {
     }
 
     @Test
-    public void oddNumbersTest(){
+    public void oddNumbersTest() {
+        //: Given
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\noodNumbers()\n1\n3\n5\n7\n9\n11\n13\n15\n17\n19";
+        //: When
+        String actual = numbers.oddNumbers();
+
+        //: Then
+        Assert.assertEquals("The two strings of odd numbers are equal", expected, actual);
+    }
+
+    @Test
+    public void squaresTest() {
+        //: Given
+        Numbers numbers = new Numbers();
+        String expected = "*** Output ***\nsquares()\n1\n4\n9\n16\n25\n36\n49\n64\n81\n100";
+
+        //: When
+        String actual = numbers.squares();
+
+        //: Then
+        Assert.assertEquals("The two strings of squares are equal", expected, actual);
+    }
+
+    @Test
+    public void random4Test() {
+        //: Given
+        Numbers numbers = new Numbers();
+        double randomNumber =  Math.random()*10;
+        String expected = "*** Output ***\nrandom4()\n3\n5\n2\n8";
+//        String expected = "*** Output ***\nrandom4()\n"+Integer.toString((int)randomNumber)+"\n"+
+//                Integer.toString((int)randomNumber)+"\n"+Integer.toString((int)randomNumber)+"\n"+Integer.toString((int)randomNumber);
+
+        //: When
+        String actual = numbers.random4();
+
+        //: Then
+        Assert.assertEquals("The two strings of random numbers are equal", expected, actual);
+    }
+
+    @Test
+    public void evenTest() {
         //: Given
 
         //: When
@@ -28,34 +69,7 @@ public class NumbersTest {
     }
 
     @Test
-    public void squaresTest(){
-        //: Given
-
-        //: When
-
-        //: Then
-    }
-
-    @Test
-    public void random4Test(){
-        //: Given
-
-        //: When
-
-        //: Then
-    }
-
-    @Test
-    public void evenTest(){
-        //: Given
-
-        //: When
-
-        //: Then
-    }
-
-    @Test
-    public void powersTest(){
+    public void powersTest() {
         //: Given
 
         //: When
